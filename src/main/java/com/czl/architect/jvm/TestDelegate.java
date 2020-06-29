205edbeca2bd0c33e1b63dbc64a636bfc45f521b
wb670938 <wb-ccl670938@antfin.com>
2020-06-29 19:10:04 +0800

    1

--- src/main/java/com/czl/architect/jvm/TestDelegate.java
+++ src/main/java/com/czl/architect/jvm/TestDelegate.java
@@ -1,5 +1,7 @@
 package com.czl.architect.jvm;
 
+import com.sun.nio.zipfs.ZipPath;
+
 /**
  * @Title: 测试双亲委派机制
  * @Description:
@@ -10,8 +12,14 @@ package com.czl.architect.jvm;
 public class TestDelegate {
     public static void main(String[] args) {
         try {
-            Class clazz = ClassLoader.getSystemClassLoader().loadClass(com.czl.architect.jvm.TestJdkClassLoader.class.getName());
-            System.out.println(clazz);
+            Class clazz = ClassLoader.getSystemClassLoader().loadClass(com.architect.jvm.TestDynamicLoad.class.getName());
+            System.out.println(clazz.getClassLoader());
+
+            Class zipClazz = ClassLoader.getSystemClassLoader().loadClass(ZipPath.class.getName());
+            System.out.println(zipClazz.getClassLoader());
+
+            Class strClazz = ClassLoader.getSystemClassLoader().loadClass(String.class.getName());
+            System.out.println(strClazz.getClassLoader());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
