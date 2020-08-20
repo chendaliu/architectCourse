503d2bfb6d6782f4e264d7e873c3b4eb7b12d1f0
wb670938 <wb-ccl670938@antfin.com>
2020-08-20 18:28:41 +0800

    1

--- src/main/java/pers/czl/architect/concurrent/CyclicBarrierTest.java
+++ src/main/java/pers/czl/architect/concurrent/CyclicBarrierTest.java
@@ -1,5 +1,7 @@
 package pers.czl.architect.concurrent;
 
+import java.util.concurrent.CyclicBarrier;
+
 /**
  * @Title:
  * @Description:
@@ -9,7 +11,44 @@ package pers.czl.architect.concurrent;
  **/
 public class CyclicBarrierTest {
 
-    public static void main(String[] args) {
+    static class TaskThread extends Thread {
 
+        CyclicBarrier barrier;
+
+        public TaskThread(CyclicBarrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+                System.out.println(getName() + " 到达栅栏 A");
+                barrier.await();
+                System.out.println(getName() + " 冲破栅栏 A");
+
+                Thread.sleep(2000);
+                System.out.println(getName() + " 到达栅栏 B");
+                barrier.await();
+                System.out.println(getName() + " 冲破栅栏 B");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int threadNum = 5;
+        CyclicBarrier barrier = new CyclicBarrier(threadNum, new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " 完成最后任务");
+            }
+        });
+
+        for(int i = 0; i < threadNum; i++) {
+            new TaskThread(barrier).start();
+        }
     }
 }
