2957588535b2fad933a68bf2b77b74cc9b7d2e68
chendaliu <794038697@qq.com>
2020-09-25 20:48:35 +0800

    1

--- src/main/java/pers/czl/spring/iocbeanlifecicle/SpringStarter.java
+++ src/main/java/pers/czl/spring/iocbeanlifecicle/SpringStarter.java
@@ -1,6 +1,7 @@
 package pers.czl.spring.iocbeanlifecicle;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,10 @@ public class SpringStarter {
 
     public static void main(String[] args) {
         new AnnotationConfigApplicationContext(SpringStarter.class);
+    }
+
+    @Bean
+    public Tank tank() {
+        return new Tank();
     }
 }
