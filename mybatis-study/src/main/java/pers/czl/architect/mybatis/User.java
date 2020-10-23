ddf70507b396478a8a5a6e9219fd97eb00bc89f7
wb670938 <wb-ccl670938@antfin.com>
2020-10-23 18:55:47 +0800

    1

---
+++ mybatis-study/src/main/java/pers/czl/architect/mybatis/User.java
@@ -0,0 +1,12 @@
+package pers.czl.architect.mybatis;
+
+/**
+ * @Title:
+ * @Description:
+ * @Author: wb-ccl670938
+ * @CreateTime: 2020-10-23 11:13
+ * @Version:1.0
+ **/
+public class User {
+
+}
