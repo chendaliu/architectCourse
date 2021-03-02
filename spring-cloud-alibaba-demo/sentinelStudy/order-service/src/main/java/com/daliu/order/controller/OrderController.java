6ed62b9721eb01e11aa7e11c8698653cd493cd65
wb670938 <wb-ccl670938@antfin.com>
2021-03-02 19:28:35 +0800

    1

--- spring-cloud-alibaba-demo/sentinelStudy/order-service/src/main/java/com/daliu/order/controller/OrderController.java
+++ spring-cloud-alibaba-demo/sentinelStudy/order-service/src/main/java/com/daliu/order/controller/OrderController.java
@@ -1,10 +1,7 @@
 package com.daliu.order.controller;
 
 
-import com.daliu.order.api.feign.stock.StockService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
-    @Autowired
-    private StockService stockService;
 
     /**
      * 基于Feign调用服务接口
@@ -22,16 +17,7 @@ public class OrderController {
      */
     @GetMapping(value = "/getStock")
     public String getStock(@RequestParam("stockId") Long stockId) {
-        return stockService.getStock(stockId);
-    }
-
-    /**
-     * 测试token传递
-     * @return
-     */
-    @GetMapping(value = "/testToken")
-    public String testToken() {
-        return stockService.testToken();
+        return "success";
     }
 
 }
