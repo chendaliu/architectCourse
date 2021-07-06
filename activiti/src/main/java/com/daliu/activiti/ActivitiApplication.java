705a819ccad400ace9484704bd958e734d7427e5
daliu <794038697@qq.com>
2021-07-06 20:08:22 +0800

    1

--- activiti/src/main/java/com/daliu/activiti/ActivitiApplication.java
+++ activiti/src/main/java/com/daliu/activiti/ActivitiApplication.java
@@ -1,10 +1,14 @@
 package com.daliu.activiti;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.activiti.spring.boot.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableAsync
+@EnableTransactionManagement //启用事务
 public class ActivitiApplication {
 
 	public static void main(String[] args) {
