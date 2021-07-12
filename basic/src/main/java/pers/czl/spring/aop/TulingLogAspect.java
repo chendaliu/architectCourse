afaab8b5a7ff994073e7bbe47bd592cc416d9512
daliu <794038697@qq.com>
2021-07-12 20:06:29 +0800

    1

--- basic/src/main/java/pers/czl/spring/aop/TulingLogAspect.java
+++ basic/src/main/java/pers/czl/spring/aop/TulingLogAspect.java
@@ -1,6 +1,9 @@
 package pers.czl.spring.aop;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import pers.czl.spring.aop.Introductions.ProgramCalculate;
@@ -14,8 +17,10 @@ import java.util.Arrays;
  */
 @Aspect
 @Order
+@Slf4j
 @Component
 public class TulingLogAspect {
+    private final static String LOG_FORMAT = "\n请求目标:\t[{}]\n请求参数:\t{}\n响应时间:\t[{}:ms]";
 
     /* 满足表达式的类会在运行时自动实现ProgramCalculate接口 */
     @DeclareParents(value="pers.czl.spring.aop.TulingCalculate",   // 动态实现的类
@@ -27,6 +32,9 @@ public class TulingLogAspect {
 
     @Pointcut("execution(* pers.czl.spring.aop.Dog.*(..))")
     public void pointCut2(){};
+
+/*    @Pointcut("execution(* com.qjdchina.web..*.*(..)) && !execution(* com.qjdchina.web.configuration..*.*(..))")
+    public void pointCut3(){};*/
 
     @Before(value = "pointCut() || pointCut2()")
     public void methodBefore(JoinPoint joinPoint) throws Throwable {
@@ -50,6 +58,21 @@ public class TulingLogAspect {
     public void methodAfterThrowing(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         System.out.println("执行目标方法【"+methodName+"】的<异常通知>,入参"+Arrays.asList(joinPoint.getArgs()));
+    }
+
+    @SneakyThrows
+    @Around("pointCut2()")
+    public Object doAround(ProceedingJoinPoint point) {
+        Long startTime = System.currentTimeMillis();
+        try {
+            return point.proceed();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw e;
+        } finally {
+            Long endTime = System.currentTimeMillis();
+            log.info(LOG_FORMAT, point.getSignature(), point.getArgs(), (endTime - startTime));
+        }
     }
 
 }
